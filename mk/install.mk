042bdd73ae6caf8592f013b71d4f4c72ba537535
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-25 10:50:51 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/install.mk
+++ mk/install.mk
@@ -11,10 +11,3 @@ Debian_install:
 Debian_update:
 	sudo apt update
 	sudo apt install -uy `cat apt.$(WS)` $(APT)
-	$(PIP) install -U    pip
-	$(PIP) install -U -r requirements.txt
-
-Msys_install: doc ref gz
-	pacman -Suy
-Msys_update:
-	pacman -S $(shell cat apt.$(WS) | tr '\n' ' ') $(MSYS)
